77863d9231abc4258fbfd95e5577147e503d4d28
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-19 12:38:53 -0300

    [Geovanna Otoni] - Lista Fundamentos Java

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -2,7 +2,30 @@ package com.trybe.java;
 
 /**
  * Classe.
- **/
+ */
 public class SmartHouse {
+  private boolean lampada = false;
 
+  /**
+   * Ligar lampada.
+   */
+  public void ligarLampada() {
+    lampada = true;
+  }
+
+  /**
+   * Desligar lampada.
+   */
+  public void desligarLampada() {
+    lampada = false;
+  }
+
+  /**
+   * Is lampada ligada boolean.
+   *
+   * @return the boolean
+   */
+  public boolean isLampadaLigada() {
+    return lampada;
+  }
 }
