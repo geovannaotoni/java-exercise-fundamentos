6c73319be5ae50d126aee76c4dcf324a59bb2053
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-19 13:14:06 -0300

    feat: classe Residente

--- src/main/java/com/trybe/java/Residente.java
+++ src/main/java/com/trybe/java/Residente.java
@@ -5,4 +5,21 @@ package com.trybe.java;
  **/
 public class Residente {
 
+  /**
+   * Atributos da classe.
+   */
+  private String nome;
+  private int idade;
+  private double peso;
+  private double altura;
+
+  /**
+   * Método Construtor.
+   */
+  public Residente(String nome, int idade, double peso, double altura) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+  }
 }
